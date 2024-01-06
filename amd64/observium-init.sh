06e6fee7a3feea0ffd8ce14531b95acf5894d876
R. Daneel Olivaw <r-daneel@trantor>
2024-01-06 12:21:37 -0500

    Enhance and optimise shell script
    Cosmetic changes and reduced complexity for better readability

--- amd64/observium-init.sh
+++ amd64/observium-init.sh
@@ -1,52 +1,59 @@
 #!/usr/bin/env bash
 
-count=0
-rc=1
+function WriteLog() {
+  echo "$(date +%Y-%m-%d\ %H:%M:%S) [$(basename ${0})] ${1}"
+  return 0
+}
 
-while [ $rc -ne 0 ]
-do
-   let count++
-   echo "[$count] Verifying coonection to observium database."
-   MYSQL_PWD="$OBSERVIUM_DB_PASS" mysql -h $OBSERVIUM_DB_HOST -u $OBSERVIUM_DB_USER -e "select 1" $OBSERVIUM_DB_NAME >/dev/null
-   rc=$?
-   [ $rc -ne 0 ] && sleep 5
-done
-
-echo "Connected to observium database successfully."
-
-tables=`MYSQL_PWD="$OBSERVIUM_DB_PASS" mysql -h $OBSERVIUM_DB_HOST -u $OBSERVIUM_DB_USER -e "show tables" $OBSERVIUM_DB_NAME 2>/dev/null`
-
-if [ -z "$tables" ]
-then
-   echo "Setting /opt/observium/rrd directory to www-data:www-data."
-   chown -v www-data:www-data /opt/observium/rrd
-   echo "Initializing database schema in first time running for observium."
-   /opt/observium/discovery.php -u
-   /opt/observium/adduser.php $OBSERVIUM_ADMIN_USER $OBSERVIUM_ADMIN_PASS 10
-else
-  echo "Database schema initialization has been done already."
-  sleep 5
-fi
-
-if [ -z "$TZ" -o "$TZ" = "Etc/UTC" ]
-then
-   echo "Keep system timezone as the default of `cat /etc/timezone`."
-else
-   if [ -f /usr/share/zoneinfo/$TZ ]
-   then
-      echo "Setting system timezone to specific $TZ."
-      echo "$TZ" > /etc/timezone
-      ln -sfv /usr/share/zoneinfo/$TZ /etc/localtime
+# dynamically enforce timezone in container
+if [ -n "${TZ}" ]; then
+   WriteLog "Set timezone to '${TZ}'"
+   if [ -f "/usr/share/zoneinfo/${TZ}" ]; then
+      echo "${TZ}" > /etc/timezone
+      ln -sf /usr/share/zoneinfo/America/Toronto /etc/localtime
    else
-      echo "Invalid specific $TZ timezone and use the default of `cat /etc/timezone` instead."
+      WriteLog "Invalid specific $TZ timezone and use the default of `cat /etc/timezone` instead."
    fi
 fi
 
-echo "export OBSERVIUM_ADMIN_USER=$OBSERVIUM_ADMIN_USER" >> /opt/observium/observium-setenv.sh
-echo "export OBSERVIUM_ADMIN_PASS=$OBSERVIUM_ADMIN_PASS" >> /opt/observium/observium-setenv.sh
-echo "export OBSERVIUM_DB_HOST=$OBSERVIUM_DB_HOST" >> /opt/observium/observium-setenv.sh
-echo "export OBSERVIUM_DB_USER=$OBSERVIUM_DB_USER" >> /opt/observium/observium-setenv.sh
-echo "export OBSERVIUM_DB_PASS=$OBSERVIUM_DB_PASS" >> /opt/observium/observium-setenv.sh
-echo "export OBSERVIUM_DB_NAME=$OBSERVIUM_DB_NAME" >> /opt/observium/observium-setenv.sh
+count=0
+while true
+do
+   let count++
+   WriteLog "Check connection to observium database (attempt #${count})"
+   MYSQL_PWD="${OBSERVIUM_DB_PASS}" mysql \
+      --host="${OBSERVIUM_DB_HOST}" \
+      --user="${OBSERVIUM_DB_USER}" \
+      --execute="select 1" \
+      "${OBSERVIUM_DB_NAME}" >/dev/null && break
+   sleep 5
+done
+WriteLog "Connection to observium database checked after ${count} attempts"
+
+# ensure rrd data is always accessible
+WriteLog "Set /opt/observium/rrd directory to www-data:www-data"
+chown -R www-data:www-data "/opt/observium/rrd"
+
+# On first run create the default schema
+# Otherwise ensure the schema is up to date with the code
+WriteLog "Init/Update database schema"
+/opt/observium/discovery.php -u
+
+# ensure the admin user is always defined
+WriteLog "Add admin user"
+/opt/observium/adduser.php "${OBSERVIUM_ADMIN_USER}" "${OBSERVIUM_ADMIN_PASS}" 10
+
+# create a reusable environment for cronjobs
+WriteLog "Build environment script for cronjobs"
+(
+   echo "export OBSERVIUM_ADMIN_USER=${OBSERVIUM_ADMIN_USER}"
+   echo "export OBSERVIUM_ADMIN_PASS=${OBSERVIUM_ADMIN_PASS}"
+   echo "export OBSERVIUM_DB_HOST=${OBSERVIUM_DB_HOST}"
+   echo "export OBSERVIUM_DB_USER=${OBSERVIUM_DB_USER}"
+   echo "export OBSERVIUM_DB_PASS=${OBSERVIUM_DB_PASS}"
+   echo "export OBSERVIUM_DB_NAME=${OBSERVIUM_DB_NAME}"
+   echo "export OBSERVIUM_BASE_URL=${OBSERVIUM_BASE_URL}"
+) > "/opt/observium/observium-setenv.sh"
+chmod 750 "/opt/observium/observium-setenv.sh"
 
 exit 0
